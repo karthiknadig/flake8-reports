d26ef8176a9fc10c63b26c78de7e4f4664ff6d92
Karthik Nadig <karthik.nadig@live.com>
2019-03-07 20:15:04 -0800

    Fix build issues

--- src/flake8junit/__init__.py
+++ src/flake8junit/__init__.py
@@ -3,6 +3,7 @@ __all__ = ['__version__', '__author__','Flake8JUnitPlugin']
 from flake8junit.version import __version__, __author__
 from flake8.formatting.base import BaseFormatter
 
+
 class Flake8JUnitPlugin(BaseFormatter):
     """Flake8's error text to JUnit XML converter."""
 
