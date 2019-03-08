39bf293260caf9e0bb34738323fd608e03902ea0
Karthik Nadig <karthik.nadig@live.com>
2019-03-07 19:45:57 -0800

    Initial code skeleton

--- src/flake8junit/__init__.py
+++ src/flake8junit/__init__.py
@@ -1,2 +1,10 @@
 
+__all__ = ['__version__', '__author__','Flake8JUnitPlugin']
 from flake8junit.version import __version__, __author__
+from flake8.formatting.base import BaseFormatter
+
+class Flake8JUnitPlugin(BaseFormatter):
+    """Flake8's error text to JUnit XML converter."""
+
+    def format(self, error):
+        return error
