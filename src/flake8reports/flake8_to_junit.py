2a0eb2567daf890e33d03687dfe3f0256cbc992a
Karthik Nadig <karthik.nadig@live.com>
2019-03-10 21:20:45 -0700

    Implament junit report formating

--- src/flake8reports/flake8_to_junit.py
+++ src/flake8reports/flake8_to_junit.py
@@ -1,49 +1,107 @@
+import sys
+
 from flake8.formatting.base import BaseFormatter
-from xml.sax.saxutils import escape
+from xml.etree import ElementTree as ET
 
 
 class Flake8junit(BaseFormatter):
     """Flake8's error text to JUnit XML converter."""
+
     def _is_error(self, code):
         return len(code) > 0 and code[0] in ('e', 'E')
 
     def beginning(self, filename):
         super(Flake8junit, self).beginning(filename)
-        self.write('    <testsuite name="{0}">'.format(filename), None)
+        assert self._testsuite is None
+        self._testsuite = ET.Element('testsuite')
+        self._testsuite.attrib['name'] = filename
 
     def finished(self, filename):
-        self.write('    </testsuite>', None)
+        assert self._testsuite is not None
+        if len(self._testsuite.items()) > 0:
+            self._root.append(self._testsuite)
+            self._testsuite = None
         super(Flake8junit, self).finished(filename)
 
     def start(self):
         super(Flake8junit, self).start()
-        self.write('<?xml version="1.0" encoding="UTF-8"?>', None)
-        self.write('<testsuites>', None)
+        self._root = ET.Element('testsuites')
+        self._testsuite = None
 
     def handle(self, error):
-        line = self.format(error)
-        self.write(line, None)
+        assert self._testsuite is not None
+        if error.code is not None:
+            testcase = ET.SubElement(self._testsuite, 'testcase')
+            testcase.attrib['classname'] = 'flake8'
+            testcase.attrib['name'] = error.filename
 
-    def format(self, error):
-        message = "{0}:{1}:{2} {3} {4}".format(
-            escape(error.filename),
-            escape(error.line_number),
-            escape(error.column_number),
-            escape(error.code),
-            escape(error.text)
-        )
-        return '''
-        <testcase classname="flake8" name="{0}">
-            <failure message="{1}" type="{2}">
-{3}
-            </failure>
-        </testcase>'''.format(
-                escape(error.code),
-                message,
-                'ERROR' if self._is_error(error.code) else 'WARNING',
-                escape(self.show_source(error)) if self.options.show_source else ''
+            failure = ET.SubElement(testcase, 'failure')
+            failure.attrib['message'] = "{0}:{1}:{2} {3} {4}".format(
+                error.filename,
+                error.line_number,
+                error.column_number,
+                error.code,
+                error.text
+            )
+            failure.attrib['type'] = 'ERROR' if self._is_error(error.code) else 'WARNING'
+
+            if self.options.show_source and error.physical_line is not None:
+                failure.text = super(Flake8junit, self).show_source(error)
+
+    def show_statistics(self, statistics):
+        testsuite = ET.Element('testsuite')
+        testsuite.attrib['name'] = 'statistics'
+
+        for error_code in statistics.error_codes():
+
+            testcase = ET.SubElement(testsuite, 'testcase')
+            testcase.attrib['classname'] = 'flake8'
+            testcase.attrib['name'] = error_code
+
+            stats_for_error_code = statistics.statistics_for(error_code)
+            statistic = next(stats_for_error_code)
+            count = statistic.count
+            count += sum(stat.count for stat in stats_for_error_code)
+            text = "{count:<5} {error_code} {message}".format(
+                count=count,
+                error_code=error_code,
+                message=statistic.message,
             )
 
+            systemout = ET.SubElement(testcase, 'system-out')
+            systemout.text = text
+
+        self._root.append(testsuite)
+
+    def show_benchmarks(self, benchmarks):
+        testsuite = ET.Element('testsuite')
+        testsuite.attrib['name'] = 'benchmarks'
+
+        float_format = "{value:<10.3} {statistic}".format
+        int_format = "{value:<10} {statistic}".format
+
+        for statistic, value in benchmarks:
+
+            testcase = ET.SubElement(testsuite, 'testcase')
+            testcase.attrib['classname'] = 'flake8'
+            testcase.attrib['name'] = statistic
+
+            if isinstance(value, int):
+                benchmark = int_format(statistic=statistic, value=value)
+            else:
+                benchmark = float_format(statistic=statistic, value=value)
+
+            systemout = ET.SubElement(testcase, 'system-out')
+            systemout.text = benchmark
+
+        self._root.append(testsuite)
+
+    def format(self, error):
+        return str(error)
+
     def stop(self):
-        self.write('</testsuites>', None)
+        res = ET.tostring(self._root)
+        if isinstance(res, bytes):
+            res = res.decode(sys.getfilesystemencoding())
+        self.write(res, None)
         super(Flake8junit, self).stop()
