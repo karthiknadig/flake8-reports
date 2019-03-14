f0fd47b2fbb88ef1bc9f681c76713180387d1220
Karthik Nadig <karthik.nadig@live.com>
2019-03-13 20:54:34 -0700

    Support json, csv, tsv, and ;sv.

--- src/flake8reports/flake8_to_json.py
+++ src/flake8reports/flake8_to_json.py
@@ -1,7 +1,59 @@
+import json
 from flake8.formatting.base import BaseFormatter
 
 
 class Flake8json(BaseFormatter):
 
+    def _is_error(self, code):
+        return len(code) > 0 and code[0] in ('e', 'E')
+
+    def start(self):
+        super(Flake8json, self).start()
+        self.errors = []
+        self.warnings = []
+        self.statistics = []
+        self.benchmarks = []
+
+    def handle(self, error):
+        if error.code is not None:
+            source = None
+            if self.options.show_source and error.physical_line is not None:
+                source = super(Flake8json, self).show_source(error)
+            obj = {
+                'filename': error.filename,
+                'line': error.line_number,
+                'column': error.column_number,
+                'code': error.code,
+                'message': error.text,
+                'source': source
+            }
+            if self._is_error(error.code):
+                self.errors.append(obj)
+            else:
+                self.warnings.append(obj)
+
+    def show_statistics(self, statistics):
+        for error_code in statistics.error_codes():
+            stats_for_error_code = statistics.statistics_for(error_code)
+            statistic = next(stats_for_error_code)
+            count = statistic.count
+            count += sum(stat.count for stat in stats_for_error_code)
+
+            self.statistics.append({'code': error_code, 'count': count, 'message': statistic.message})
+
+    def show_benchmarks(self, benchmarks):
+        for benchmark, value in benchmarks:
+            self.benchmarks.append({'benchmark': benchmark, 'value': value})
+
     def format(self, error):
-        return error
+        return str(error)
+
+    def stop(self):
+        obj = {
+            'errors': self.errors,
+            'warnings': self.warnings,
+            'statistics': self.statistics,
+            'benchmarks': self.benchmarks
+        }
+        self.write(json.dumps(obj), None)
+        super(Flake8json, self).stop()
--- src/flake8reports/flake8_to_xsv.py
+++ src/flake8reports/flake8_to_xsv.py
@@ -1,31 +1,65 @@
+import csv
 from flake8.formatting.base import BaseFormatter
+try:
+    import StringIO
+except:
+    import io as StringIO
 
 
 class Flake8xsv(BaseFormatter):
     def __init__(self, options):
-        self._separator = ','
-        super(Flake8xsv, self).__init__()
+        self._dialect = 'excel'
+        super(Flake8xsv, self).__init__(options)
+
+    def start(self):
+        super(Flake8xsv, self).start()
+        self.rows = []
+
+    def handle(self, error):
+        if error.code is not None:
+            row = [error.filename, error.line_number, error.column_number, error.code, error.text]
+
+            if self.options.show_source and error.physical_line is not None:
+                row += [super(Flake8xsv, self).show_source(error)]
+
+            self.rows.append(row)
+
+    def show_statistics(self, statistics):
+        self.rows.append(['statistics'])
+        for error_code in statistics.error_codes():
+            stats_for_error_code = statistics.statistics_for(error_code)
+            statistic = next(stats_for_error_code)
+            count = statistic.count
+            count += sum(stat.count for stat in stats_for_error_code)
+            self.rows.append([error_code, count, statistic.message])
+
+    def show_benchmarks(self, benchmarks):
+        self.rows.append(['benchmarks'])
+        for benchmark, value in benchmarks:
+            self.rows.append([benchmark, value])
 
     def format(self, error):
-        return error
+        return str(error)
 
-class Flake8csv(BaseFormatter):
+    def stop(self):
+        str_stream = StringIO.StringIO()
+        csv_stream = csv.writer(str_stream, dialect=self._dialect)
+        csv_stream.writerows(self.rows)
+        self.write(str_stream.getvalue(), None)
+        super(Flake8xsv, self).stop()
+
+
+class Flake8csv(Flake8xsv):
     def after_init(self):
-        self._separator = ','
+        self._dialect = csv.excel
 
-    def format(self, error):
-        return error
 
-class Flake8tsv(BaseFormatter):
+class Flake8tsv(Flake8xsv):
     def after_init(self):
-        self._separator = '\t'
+        self._dialect = csv.excel_tab
 
-    def format(self, error):
-        return error
 
-class Flake8ssv(BaseFormatter):
+class Flake8ssv(Flake8xsv):
     def after_init(self):
-        self._separator = ';'
-
-    def format(self, error):
-        return error
+        csv.register_dialect('semicolon', delimiter=';', quoting=csv.QUOTE_MINIMAL)
+        self._dialect = 'semicolon'
